21389399462f9d3c689167822436bbe33fbbc048
artanderson <arthuranders@gmail.com>
2021-09-10 17:51:16 -0700

    add script

--- filter.lua
+++ filter.lua
@@ -1,8 +1,10 @@
-function Meta (m)
-  m.title = "title"    
-  return m
-end    
+--This part doesn't work yet
+--function Meta (m)
+--  m.title = "title"    
+--  return m
+--end    
 
+--Removes TOC, converts code output to BlockQuote and removes extra div tags  
 function Div (elem)
   if elem.attributes['toc'] == 'true' then
     elem.content = {}   
@@ -12,12 +14,14 @@ function Div (elem)
   return elem.content     
 end
 
+--Replaces inline <pre> tags with inline code
 function RawInline (raw)
   if raw.format == 'html' and raw.text == '<pre>' or raw.text == '</pre>' then
     return pandoc.RawInline('markdown', '\n```\n')
   end
 end
 
+--Replaces block <pre> tags with code blocks
 function RawBlock (raw)
   if raw.format == 'html' then
     return pandoc.CodeBlock(raw.text)
